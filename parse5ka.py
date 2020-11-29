a8825f1841dff55318850c7c83e03b62198cc0f9
Grigory Rodimov <grishanyar@gmail.com>
2020-11-29 19:48:39 +0300

    les1 code

---
+++ parse5ka.py
@@ -0,0 +1,58 @@
+import os
+from pathlib import Path
+import json
+import time
+
+import requests
+
+
+class Parse5ka:
+    _headers = {
+        'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36",
+    }
+
+    _params = {
+        'records_per_page': 50,
+    }
+
+    def __init__(self, start_url):
+        self.start_url = start_url
+
+    @staticmethod
+    def _get(*args, **kwargs) -> requests.Response:
+        while True:
+            try:
+                response = requests.get(*args, **kwargs)
+                if response.status_code != 200:
+                    raise Exception
+                return response
+            except Exception:
+                time.sleep(0.25)
+
+    def parse(self, url):
+        params = self._params
+        while url:
+            response: requests.Response = self._get(url, params=params, headers=self._headers)
+            if params:
+                params = {}
+            data: dict = response.json()
+            url = data.get('next')
+            yield data.get('results')
+
+    def run(self):
+        for products in self.parse(self.start_url):
+            for product in products:
+                self._save_to_file(product)
+            time.sleep(0.1)
+
+    @staticmethod
+    def _save_to_file(product):
+        path = Path(os.path.dirname(__file__)).joinpath('products').joinpath(f'{product["id"]}.json')
+        with open(path, 'w', encoding='UTF-8') as file:
+            json.dump(product, file, ensure_ascii=False)
+
+
+
+if __name__ == '__main__':
+    parser = Parse5ka('https://5ka.ru/api/v2/special_offers/')
+    parser.run()
